c0d16db35ba8c1ddd3eec4d241fa3f1491de3aec
Mamed766 <mehemmeddemirli57@gmail.com>
2024-10-02 15:23:05 +0400

    refactored

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Dropdown = ({ items }) => {
   return (
-    <div className="absolute left-0 top-[100px] w-full z-10 bg-white p-5 shadow-xl group-hover:scale-y-100 transform scale-y-0 origin-top transition-transform duration-300 ease-in-out">
+    <div className="absolute left-0 top-[80px] w-full z-10 bg-white p-5 shadow-xl group-hover:scale-y-100 transform scale-y-0 origin-top transition-transform duration-300 ease-in-out">
       <div className="flex flex-wrap justify-center gap-20">
         {items.map((column, index) => (
           <div key={index} className="flex flex-col">
